e524f97cb53dbcabd899e229ea58704f255244b4
Brajananda Mahata <brajanandamahato280603@gmail.com>
2025-04-20 10:27:53 +0530

    adding first static build

---
+++ _app/immutable/nodes/0.owNPFlT5.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/CaasTpf9.js";import{$ as p}from"../chunks/EW6ToMZV.js";import{s}from"../chunks/BXh0c7lr.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
