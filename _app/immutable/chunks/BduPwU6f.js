51cd00031e3e565c95efba20b0851489ec2c323b
Brajananda Mahata <brajanandamahato280603@gmail.com>
2025-04-20 13:16:21 +0530

    add pdf-splitting and menu-bar

--- _app/immutable/chunks/BduPwU6f.js
+++
@@ -1,1 +0,0 @@
-import{k as n,g as e,l as s}from"./EW6ToMZV.js";function u(t){var r=s(0);return function(){return arguments.length===1?(n(r,e(r)+1),arguments[0]):(e(r),t())}}export{u as r};
---
+++ _app/immutable/chunks/C3ME5kde.js
@@ -0,0 +1,20 @@
+import"./CWj6FrbW.js";import{i as S}from"./WWVtWIcG.js";import{h as B,a as I,e as q,j as A,F,s as O,r as P,t as k,i as D,f as E,g as b}from"./4Vjq8Xjj.js";import{n as G,c as H,a as w}from"./B4dHhGZh.js";import{e as J,a as K,i as L,b as x}from"./yOqKe9X7.js";import{l as W,p as o}from"./BQESZp3Y.js";function M(c,e,n,l,f){var r;B&&I();var s=(r=e.$$slots)==null?void 0:r[n],a=!1;s===!0&&(s=e.children,a=!0),s===void 0||s(c,a?()=>l:l)}/**
+ * @license lucide-svelte v0.501.0 - ISC
+ *
+ * ISC License
+ * 
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ */const Q={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};var R=G("<svg><!><!></svg>");function ee(c,e){const n=W(e,["children","$$slots","$$events","$$legacy"]),l=W(n,["name","color","size","strokeWidth","absoluteStrokeWidth","iconNode"]);q(e,!1);let f=o(e,"name",8,void 0),s=o(e,"color",8,"currentColor"),a=o(e,"size",8,24),r=o(e,"strokeWidth",8,2),p=o(e,"absoluteStrokeWidth",8,!1),y=o(e,"iconNode",24,()=>[]);const z=(...i)=>i.filter((t,u,h)=>!!t&&h.indexOf(t)===u).join(" ");S();var d=R();let m;var g=A(d);J(g,1,y,L,(i,t)=>{let u=()=>b(t)[0],h=()=>b(t)[1];var v=H(),j=F(v);K(j,u,!0,(C,T)=>{let _;k(()=>_=x(C,_,{...h()}))}),w(i,v)});var N=O(g);M(N,e,"default",{}),P(d),k((i,t)=>m=x(d,m,{...Q,...l,width:a(),height:a(),stroke:s(),"stroke-width":i,class:t}),[()=>p()?Number(r())*24/Number(a()):r(),()=>z("lucide-icon","lucide",f()?`lucide-${f()}`:"",n.class)],D),w(c,d),E()}export{ee as I,M as s};
